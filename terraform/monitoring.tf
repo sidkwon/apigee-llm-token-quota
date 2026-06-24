16e2aaeed4e53a347cc95a8099a90cf04b3ef71f
sidkwon <annakie.kwon@gmail.com>
2026-06-24 02:21:13 +0000

    fix(monitoring): use MQL query in Top 10 table with matching column alias user_email to fix raw distribution JSON dump

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -261,7 +261,7 @@ resource "google_monitoring_dashboard" "llm_dashboard" {
         "timeSeriesTable": {
           "columnSettings": [
             {
-              "column": "metric.label.user_email",
+              "column": "user_email",
               "visible": true,
               "displayName": "User Email"
             },
@@ -274,17 +274,7 @@ resource "google_monitoring_dashboard" "llm_dashboard" {
           "dataSets": [
             {
               "timeSeriesQuery": {
-                "timeSeriesFilter": {
-                  "filter": "metric.type=\"logging.googleapis.com/user/apigee_llm_total_tokens\"",
-                  "aggregation": {
-                    "alignmentPeriod": "60s",
-                    "perSeriesAligner": "ALIGN_DELTA",
-                    "crossSeriesReducer": "REDUCE_SUM",
-                    "groupByFields": [
-                      "metric.label.user_email"
-                    ]
-                  }
-                }
+                "timeSeriesQueryLanguage": "fetch global | metric 'logging.googleapis.com/user/apigee_llm_total_tokens' | align | group_by [user_email: metric.user_email], sum(val()) | top 10"
               }
             }
           ]
